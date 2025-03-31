dcda3dbd96c75542eb8de193ceea5553ae740af8
Eric Hill <ehill@zillowgroup.com>
2025-03-31 18:54:10 -0400

    Add toggle all airport checkbox

--- app/src/app/trip/components/RouteFilters.tsx
+++ app/src/app/trip/components/RouteFilters.tsx
@@ -24,6 +24,10 @@ export default function RouteFilters({
 }: RouteFiltersProps) {
   const [isOpen, setIsOpen] = useState(false);
 
+  const hasValidFilters =
+    filterState.filteredDepartureAirports.some((c) => c.isSelected) &&
+    filterState.filteredArrivalAirports.some((c) => c.isSelected);
+
   const handleApplyFilters = () => {
     applyFilters();
     setIsOpen(false);
@@ -32,77 +36,51 @@ export default function RouteFilters({
   return (
     <ResponsiveDialog
       title='Filters'
-      Trigger={<FilterButton isOpen={isOpen} setIsOpen={setIsOpen} />}
+      Trigger={
+        <Button size='lg' onClick={() => setIsOpen(!isOpen)}>
+          <ListFilter />
+        </Button>
+      }
       Content={
-        <RouteFiltersContent
-          trip={trip}
-          filterState={filterState}
-          setFilterState={setFilterState}
-        />
+        <div>
+          <div className='flex justify-start space-x-2 mt-4 mb-2'>
+            <FareClassButton
+              fareClass='economy'
+              filterState={filterState}
+              setFilterState={setFilterState}
+            />
+            <FareClassButton
+              fareClass='business'
+              filterState={filterState}
+              setFilterState={setFilterState}
+            />
+          </div>
+          <div className='grid grid-cols-2 gap-2'>
+            <AirportList
+              title='Departure Airports'
+              airports={trip.departureAirports}
+              filterState={filterState}
+              setFilterState={setFilterState}
+            />
+            <AirportList
+              title='Arrival Airports'
+              airports={trip.arrivalAirports}
+              filterState={filterState}
+              setFilterState={setFilterState}
+            />
+          </div>
+        </div>
       }
       Footer={
         <div className='flex justify-end'>
-          <Button onClick={handleApplyFilters}>Apply</Button>
+          <Button disabled={!hasValidFilters} onClick={handleApplyFilters}>
+            Apply
+          </Button>
         </div>
       }
       isOpen={isOpen}
       setIsOpen={setIsOpen}
     />
-  );
-}
-
-function FilterButton({
-  isOpen,
-  setIsOpen,
-}: {
-  isOpen: boolean;
-  setIsOpen: (isOpen: boolean) => void;
-}) {
-  return (
-    <Button size='lg' onClick={() => setIsOpen(!isOpen)}>
-      <ListFilter />
-    </Button>
-  );
-}
-
-function RouteFiltersContent({
-  trip,
-  filterState,
-  setFilterState,
-}: {
-  trip: Trip;
-  filterState: FilterState;
-  setFilterState: (filterState: FilterState) => void;
-}) {
-  return (
-    <div>
-      <div className='flex justify-start space-x-2 mt-4 mb-2'>
-        <FareClassButton
-          fareClass='economy'
-          filterState={filterState}
-          setFilterState={setFilterState}
-        />
-        <FareClassButton
-          fareClass='business'
-          filterState={filterState}
-          setFilterState={setFilterState}
-        />
-      </div>
-      <div className='grid grid-cols-2 gap-2'>
-        <AirportList
-          title='Departure Airports'
-          airports={trip.departureAirports}
-          filterState={filterState}
-          setFilterState={setFilterState}
-        />
-        <AirportList
-          title='Arrival Airports'
-          airports={trip.arrivalAirports}
-          filterState={filterState}
-          setFilterState={setFilterState}
-        />
-      </div>
-    </div>
   );
 }
 
@@ -153,12 +131,35 @@ function AirportList({
     });
   };
 
+  const handleAllAirportsCheckBoxChange = (checked: boolean) => {
+    setFilterState({
+      ...filterState,
+      [filterStateKey]: filterState[filterStateKey].map((c) => ({
+        ...c,
+        isSelected: checked,
+      })),
+    });
+  };
   return (
     <div className=' p-2'>
-      <h3 className='text-lg font-bold'>{title}</h3>
       <ul>
+        <li className='flex items-center gap-2 mb-2'>
+          <Checkbox
+            id='all'
+            checked={filterState[filterStateKey].every((c) => c.isSelected)}
+            onCheckedChange={(checked) =>
+              handleAllAirportsCheckBoxChange(checked)
+            }
+          />
+          <label htmlFor='all'>
+            <h3 className='text-lg font-bold'>{title}</h3>
+          </label>
+        </li>
         {airports.map((airport) => (
-          <li key={airport.id} className='flex items-center gap-2'>
+          <li
+            key={airport.id}
+            className='flex items-center gap-2  hover:bg-gray-100 rounded-md'
+          >
             <Checkbox
               id={airport.id.toString()}
               checked={
@@ -168,7 +169,9 @@ function AirportList({
               }
               onCheckedChange={() => handleAirportsCheckBoxChange(airport)}
             />
-            <label htmlFor={airport.id.toString()}>{airport.iata}</label>
+            <label className='w-full' htmlFor={airport.id.toString()}>
+              {airport.iata}
+            </label>
           </li>
         ))}
       </ul>
